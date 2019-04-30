5e1914b29726769f92198771152cf568de54a035
Ahmed Shatat <a.shatat@hotmail.com>
2019-05-01 00:57:09 +0300

    feat: SearchBar component, axios configuration

---
+++ src/components/App.js
@@ -0,0 +1,28 @@
+import React, {Component} from 'react';
+import SearchBar from './SearchBar';
+import youtube from '../apis/youtube';
+
+class App extends Component {
+  state = {
+    videos: []
+  }
+  onTermSubmit = async (term) => {
+    const response = await youtube.get('/search', {
+      params: {
+        q: term
+      }
+    })
+    
+    this.setState({ videos: response.data.items})
+  }
+
+  render() {
+    return (
+      <div className="ui container">
+        <SearchBar onFormSubmit={this.onTermSubmit} />
+      </div>
+    )
+  }
+}
+
+export default App;
